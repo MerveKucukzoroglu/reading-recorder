478d9a8681c58ae7939bc430b8433c5812362cc4
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2021-11-06 15:41:30 +0000

    Validate menu and direct user  from option chosen

--- run.py
+++ run.py
@@ -23,7 +23,26 @@ def menu():
     print("1. Log-in a book")
     print("2. About Reading-Tracker\n")
 
-    menu_chosen = int(input("Enter '1' or '2' from the menu to continue:"))
-    print(f"You chose {menu_chosen}")
+    menu_chosen = input("Enter '1' or '2' from the menu to continue: ")
+
+    validate_menu(menu_chosen)
+
+def validate_menu(value):
+    """
+    Direct user to option chosen from the menu.
+    Let the user enter '1' or '2' else print an error.
+    """
+    if value == "1":
+        print("Call log-in a book function")
+    elif value == "2":
+        print("This is a tracker to keep a track on the books you read in a time period.")
+        print("You will be asked to enter name of the book and name of the author")
+        print("You will enter the date you started and completed the book")
+        print("You can also use this tracker to enter future dates and make a record of when you want to read this book")
+    elif value == "":
+        print("Please choose an option from the menu")    
+    else:
+        print(f"Option chosen is not in the menu, you chose {value}.\nPlease enter '1' or '2'")
+              
 
 menu()
