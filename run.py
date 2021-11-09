4dc0f52a9a66634f44a94ab21c44c9772f8519f6
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2021-11-09 13:02:58 +0000

    Create submit_book and about functions for menu

--- run.py
+++ run.py
@@ -31,6 +31,24 @@ def menu():
             break
     return menu_chosen
 
+def submit_book():
+    """
+    Initial function to begin collecting data from user
+    """
+    print("It's great to see you submitting a book. Please follow the steps to successfully log-in a book!")
+
+def about():
+    """
+    About the program from menu
+    Either exits the about section or 
+    The user submits a book
+    """
+    print("This is a tracker to keep a track on the books you read in a time period.")
+    print("You will be asked to enter name of the book and name of the author")
+    print("You will enter the date you started and completed the book")
+    print("You can also use this tracker to enter future dates and make a record of when you want to read this book\n")
+    
+    choose_about = input("Would you like to submit a book?\nType 'y' to continue or 'n' to exit: ")    
 
 def validate_menu(value):
     """
@@ -38,14 +56,12 @@ def validate_menu(value):
     Let the user enter '1' or '2' else print an error.
     """
     if value == "1":
-        print("Call log-in a book function")
+        print("Calling submit_book function...")
+        submit_book()
     elif value == "2":
-        print("This is a tracker to keep a track on the books you read in a time period.")
-        print("You will be asked to enter name of the book and name of the author")
-        print("You will enter the date you started and completed the book")
-        print("You can also use this tracker to enter future dates and make a record of when you want to read this book")
+        about()
     elif value == "":
-        print("Please choose an option from the menu") 
+        print("Please choose a valid option") 
         return False   
     else:
         print(f"Option chosen is not in the menu, you chose {value}.\nPlease enter '1' or '2'\n")
